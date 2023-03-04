38380dbaddd5021f4b7dba9bf5a21b1e84dfd392
pratiksha-dse <b20165@students.iitmandi.ac.in>
2023-03-04 14:25:08 +0530

    rider status

--- lib/screens/rideStatus.dart
+++ lib/screens/rideStatus.dart
@@ -15,7 +15,9 @@ class _RideStatusState extends State<RideStatus> {
   LatLng currentLocation = getCurrentLatLngFromSharedPrefs();
   late String currentAddress;
   late CameraPosition _initialCameraPosition;
-  bool isAccepted=false;
+  bool isAccepted = true;
+  bool started = false;
+  bool completed = true;
 
   @override
   void initState() {
@@ -23,66 +25,128 @@ class _RideStatusState extends State<RideStatus> {
     _initialCameraPosition = CameraPosition(target: currentLocation, zoom: 14);
     currentAddress = getCurrentAddressFromSharedPrefs();
     print("access");
-    print( dotenv.env['MAPBOX_ACCESS_TOKEN']);
-    
+    print(dotenv.env['MAPBOX_ACCESS_TOKEN']);
   }
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Ride Status")),
-        body: Stack(
-      children: [
-        // MapboxMap(
-        //   initialCameraPosition: _initialCameraPosition,
-        //   accessToken:dotenv.env['MAPBOX_ACCESS_TOKEN'],
-        //   myLocationEnabled: true,
-        // ),
-       SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: Card(
-              clipBehavior: Clip.antiAlias,
-              child: Padding(
-                padding: const EdgeInsets.all(15),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Row(children:[
-                        if(isAccepted)
-                        Icon(Icons.check_circle_outline_outlined,color:Colors.green),
-                        Icon(Icons.circle_outlined),
-                        SizedBox(width:10),
-                      const Text(
-                        'Looking for Cab',
-                        style: TextStyle(
-                          color:Colors.grey,
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      )]),
-                      const SizedBox(height: 20),
-                      const Text('You are currently here:'),
-                      Text(currentAddress,
-                          style: const TextStyle(color: Color.fromRGBO(255, 114, 94, 1))),
-                      const SizedBox(height: 20),
-                      ElevatedButton(
-                          onPressed: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (_) => const PrepareRide())),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color.fromRGBO(255, 114, 94, 1),
-                              padding: const EdgeInsets.all(20)),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text('Where do you wanna go today?'),
-                              ])),
-                    ]),
-              ),
+        appBar: AppBar(title: Text("Ride Status")),
+        body: Column(
+          children: [
+            // MapboxMap(
+            //   initialCameraPosition: _initialCameraPosition,
+            //   accessToken:dotenv.env['MAPBOX_ACCESS_TOKEN'],
+            //   myLocationEnabled: true,
+            // ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width * .1,
+                      top: MediaQuery.of(context).size.width * .1),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      // mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IntrinsicHeight(
+                            child: Row(children: [
+                          if (!isAccepted)
+                            Icon(Icons.check_circle_outline_outlined,
+                                color: Colors.green),
+                          if (isAccepted) Icon(Icons.circle_outlined),
+                          SizedBox(width: 10),
+                          const Text(
+                            'Looking for Cab',
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 103, 102, 102),
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ])),
+                        Container(
+                            padding: EdgeInsets.only(left: 10),
+                            height: 80,
+                            child:
+                                VerticalDivider(width: 2, color: Colors.grey)),
+                        const SizedBox(height: 20),
+                        IntrinsicHeight(
+                            child: Row(children: [
+                          if (isAccepted)
+                            Icon(Icons.check_circle_outline_outlined,
+                                color: Colors.green),
+                          if(!isAccepted)Icon(Icons.circle_outlined),
+                          const Text(
+                            'Driver Approaching',
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 103, 102, 102),
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ])),
+                        Container(
+                            padding: EdgeInsets.only(left: 10),
+                            height: 80,
+                            child:
+                                VerticalDivider(width: 2, color: Colors.grey)),
+                        IntrinsicHeight(
+                            child: Row(children: [
+                          if (started)
+                            Icon(Icons.check_circle_outline_outlined,
+                                color: Colors.green),
+                          if(!started)Icon(Icons.circle_outlined),
+                          SizedBox(width: 10),
+                          const Text(
+                            'Picked Up',
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 103, 102, 102),
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ])),
+                        Container(
+                            padding: EdgeInsets.only(left: 10),
+                            height: 80,
+                            child:
+                                VerticalDivider(width: 2, color: Colors.grey)),
+                        IntrinsicHeight(
+                            child: Row(children: [
+                          if (completed)
+                            Icon(Icons.check_circle_outline_outlined,
+                                color: Colors.green),
+                          if(!completed)Icon(Icons.circle_outlined),
+                          SizedBox(width: 10),
+                          const Text(
+                            'Dropped Off',
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 103, 102, 102),
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ])),
+                      ]),
+                ),
             ),
-          ),
-      ],
-    ));
+            if (completed) SizedBox(height: 25),
+            if (completed)
+              ElevatedButton(
+                  onPressed: () => Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => const PrepareRide())),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(255, 114, 94, 1),
+                      padding: const EdgeInsets.all(20)),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * .4,
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(30)),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text('Pay Now'),
+                        ]),
+                  )),
+          ],
+        ));
   }
 }
