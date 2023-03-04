8aa537557b1929c51e69ff5518cacd39353618e2
pratiksha-dse <b20165@students.iitmandi.ac.in>
2023-03-05 00:24:08 +0530

    UI improved

--- lib/screens/currentRide.dart
+++ lib/screens/currentRide.dart
@@ -4,7 +4,9 @@ import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:dola/helpers/shared_prefs.dart';
 
 class CurrentRide extends StatefulWidget {
-  const CurrentRide({Key? key, required this.riderAdd,required this.destination}) : super(key: key);
+  const CurrentRide(
+      {Key? key, required this.riderAdd, required this.destination})
+      : super(key: key);
   final String riderAdd;
   final String destination;
   @override
@@ -16,6 +18,8 @@ class _CurrentRideState extends State<CurrentRide> {
   String destination = "Mumbai";
   String fare = "100";
   String date = "1/1/23";
+  bool pickedUp = false;
+  bool droppedOff = false;
   @override
   void initState() {
     super.initState();
@@ -25,56 +29,125 @@ class _CurrentRideState extends State<CurrentRide> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Current Ride")),
-      body: SingleChildScrollView(
+      body: Center(
           child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              // mainAxisSize: MainAxisSize.min,
               children: [
-              Padding(
-                  padding: EdgeInsets.only(left:17,right:17,top:15),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width*.8,
-                      padding: EdgeInsets.all(20),
-                      // height: 100,
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 110, 135, 194),
-                          borderRadius: BorderRadius.circular(15)),
-                      child: Column(
-                        children:[
-                    
-                              Text("Source:$source",style:TextStyle(fontSize: 15,fontWeight: FontWeight.w600)),
-                              Text("Fare:",style:TextStyle(fontSize: 15,fontWeight: FontWeight.w600)),
-                              Text("Destination:$destination",style:TextStyle(fontSize: 15,fontWeight: FontWeight.w600)),
-                         
-                        Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        child: ElevatedButton(
-                          onPressed: () async {
-                          
-                          },
-                          style: ButtonStyle(
-                              backgroundColor:
-                                   MaterialStatePropertyAll<Color>(
-                                Colors.blue[900]!,
-                              ),
-                              shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ))),
-                          child: Container(
-                              padding: const EdgeInsets.all(12),
-                              // width: MediaQuery.of(context).size.width * 0.,
-                              child: Text(
-                                'Accept Ride Request',
-                                style: TextStyle(
-                                    fontSize: 10, fontWeight: FontWeight.w600),
-                                textAlign: TextAlign.center,
-                              )),
-                        ),
+            Padding(
+                padding: EdgeInsets.only( top: 15),
+                child: Container(
+                    width: MediaQuery.of(context).size.width * .8,
+                    padding: EdgeInsets.all(20),
+                    // height: 100,
+                    decoration: BoxDecoration(
+                        boxShadow: const [
+                           BoxShadow(
+                            color: Color.fromARGB(255, 99, 97, 97),
+                            offset: const Offset(
+                        5.0,
+                        5.0,
                       ),
-                      ]))),
-                       Container(height:50, child:VerticalDivider(color: Colors.blue,)),
+                      blurRadius: 10.0,
+                      spreadRadius: 2.0,
+                          )
+                        ],
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                      Text("Source: $source",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w600)),
+                      Text("Fare: ",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w600)),
+                      Text("Destination: $destination",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w600)),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                        if (pickedUp)
+                          Icon(Icons.check_circle_outline_outlined,
+                              color: Colors.green),
+                        Container(
+                          alignment: Alignment.center,
+                          //  width: MediaQuery.of(context).size.width * .8,
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              setState(() {
+                                pickedUp = true;
+                              });
+                            },
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStatePropertyAll<Color>(
+                                  Colors.blue[900]!,
+                                ),
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ))),
+                            child: Container(
+                                padding: const EdgeInsets.all(12),
+                                // width: MediaQuery.of(context).size.width * 0.,
+                                child: Text(
+                                  'Picked Up',
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w600),
+                                  textAlign: TextAlign.center,
+                                )),
+                          ),
+                        ),
+                      ]),
+                      Row(
+                         crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                        if (droppedOff)
+                          Icon(Icons.check_circle_outline_outlined,
+                              color: Colors.green),
+                       Container(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              setState(() {
+                                droppedOff = true;
+                              });
+                            },
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStatePropertyAll<Color>(
+                                  Colors.blue[900]!,
+                                ),
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ))),
+                            child: Container(
+                                padding: const EdgeInsets.all(12),
+                                // width: MediaQuery.of(context).size.width * 0.,
+                                child: Text(
+                                  'Dropped Off',
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w600),
+                                  textAlign: TextAlign.center,
+                                )),
+                          ),
+                        ),
+                      ])
+                    ]))),
+          
           ])),
     );
   }
--- lib/screens/incomingRides.dart
+++ lib/screens/incomingRides.dart
@@ -3,6 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:dola/helpers/shared_prefs.dart';
 import 'package:dola/screens/currentRide.dart';
+
 class IncomingRides extends StatefulWidget {
   const IncomingRides({Key? key}) : super(key: key);
 
@@ -15,6 +16,8 @@ class _IncomingRidesState extends State<IncomingRides> {
   String destination = "Mumbai";
   String fare = "100";
   String date = "1/1/23";
+  String rideReq = "Accept Ride Request";
+  bool isDisabled = false;
   @override
   void initState() {
     super.initState();
@@ -23,7 +26,7 @@ class _IncomingRidesState extends State<IncomingRides> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Your Past Rides")),
+      appBar: AppBar(title: Text("Incoming Rides")),
       body: SingleChildScrollView(
           child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -31,70 +34,96 @@ class _IncomingRidesState extends State<IncomingRides> {
               children: [
             for (var i = 0; i < 10; i++)
               Padding(
-                  padding: EdgeInsets.only(left:17,right:17,top:15),
+                  padding: EdgeInsets.only(left: 17, right: 17, top: 15),
                   child: Container(
                       padding: EdgeInsets.all(20),
                       // height: 100,
                       decoration: BoxDecoration(
                           color: Color.fromARGB(255, 203, 203, 203),
                           borderRadius: BorderRadius.circular(15)),
-                      child: Column(
-                        children:[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text("Source:$source",style:TextStyle(fontSize: 12,fontWeight: FontWeight.w600)),
-                              Text("Fare:$fare",style:TextStyle(fontSize: 12,fontWeight: FontWeight.w600)),
-                            ],
-                          ),
-                          
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text("Destination:$destination",style:TextStyle(fontSize: 12,fontWeight: FontWeight.w600)),
-                              Text("Date:$date",style:TextStyle(fontSize: 12,fontWeight: FontWeight.w600)),
-                            ],
-                          ),
-                       
-                        ],
-                      ),
-                        Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          const CurrentRide(riderAdd:"Delhi",destination:"Mumbai")),
-                                );
-                          },
-                          style: ButtonStyle(
-                              backgroundColor:
-                                   MaterialStatePropertyAll<Color>(
-                                Colors.blue[900]!,
-                              ),
-                              shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ))),
-                          child: Container(
-                              padding: const EdgeInsets.all(12),
-                              // width: MediaQuery.of(context).size.width * 0.,
-                              child: Text(
-                                'Accept Ride Request',
-                                style: TextStyle(
-                                    fontSize: 10, fontWeight: FontWeight.w600),
-                                textAlign: TextAlign.center,
-                              )),
+                      child: Column(children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text("Source:$source",
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600)),
+                                Text("Fare:$fare",
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600)),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text("Destination:$destination",
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600)),
+                                Text("Date:$date",
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600)),
+                              ],
+                            ),
+                          ],
                         ),
-                      ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              print("btn status");
+                              print(isDisabled);
+                              isDisabled
+                                  ? null
+                                  : {
+                                      setState(() {
+                                        rideReq = "Ride Accepted";
+                                        isDisabled = true;
+                                      }),
+                                      Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const CurrentRide(
+                                                    riderAdd: "Delhi",
+                                                    destination: "Mumbai")),
+                                      )
+                                    };
+                            },
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStatePropertyAll<Color>(
+                                  Colors.blue[900]!,
+                                ),
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ))),
+                            child: Container(
+                                padding: const EdgeInsets.all(12),
+                                // width: MediaQuery.of(context).size.width * 0.,
+                                child: Text(
+                                  rideReq,
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w600),
+                                  textAlign: TextAlign.center,
+                                )),
+                          ),
+                        ),
                       ]))),
-                       Container(height:50, child:VerticalDivider(color: Colors.blue,)),
+            Container(
+                height: 50,
+                child: VerticalDivider(
+                  color: Colors.blue,
+                )),
           ])),
     );
   }
